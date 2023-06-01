c1080c3d243c0e16f68f79bb8f698177daaa5099
Jay <sinaelj@gmail.com>
2023-06-01 23:52:38 +0900

    ➕ React 기본 설정

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import GlobalStyle from './Styles/GlobalStyle';
+import ProductList from './Components/Product/ProductList';
+
+function App() {
+  return (
+    <div>
+      <GlobalStyle />
+      hello world
+      <ProductList />
+    </div>
+  );
+}
+export default App;
