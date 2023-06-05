e4afe5bff26f0986f0f3bf102ea8532856199d4c
Jay <sinaelj@gmail.com>
2023-06-05 23:58:43 +0900

    ♻️ 상품 리스트 컴포넌트 분리

--- src/Components/Product/ProductList.jsx
+++ src/Components/Product/ProductList.jsx
@@ -1,37 +1,39 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+import styled from 'styled-components';
 import FetchComponent from '../common/FetchComponent';
 import { A11yHidden } from '../common/A11yHidden';
-import styled from 'styled-components';
-import Header from '../common/Header';
 
-const ProductList = () => (
-    <>
-        <Header />
-        <FetchComponent url="products/">
-            {(data) => (
-                <ProductItem>
-                    <h2>
-                        <A11yHidden>상품리스트</A11yHidden>
-                    </h2>
-                    {data && data.results && Array.isArray(data.results) ? (
-                        data.results.map((product) => (
-                            <article key={product.product_id}>
-                                <span>
-                                    <img src={product.image} alt={product.product_name} />
-                                </span>
-                                <p>{product.store_name}</p>
-                                <h3>{product.product_name}</h3>
-                                <h4><strong>{product.price}</strong>원</h4>
-                            </article>
-                        ))
-                    ) : (
-                        <div>상품이 없습니다.</div>
-                    )}
-                </ProductItem>
-            )}
-        </FetchComponent>
-    </>
-);
+const ProductList = () => {
+
+    return (
+        <>
+            <FetchComponent url="products/">
+                {(data) => (
+                    <ProductItem>
+                        <h2>
+                            <A11yHidden>상품리스트</A11yHidden>
+                        </h2>
+                        {data && data.results && Array.isArray(data.results) ? (
+                            data.results.map((product) => (
+                                <article key={product.product_id}>
+                                    <span>
+                                        <img src={product.image} alt={product.product_name} />
+                                    </span>
+                                    <p>{product.store_name}</p>
+                                    <h3>{product.product_name}</h3>
+                                    <h4><strong>{product.price}</strong>원</h4>
+                                </article>
+                            ))
+                        ) : (
+                            <div>상품이 없습니다.</div>
+                        )}
+                    </ProductItem>
+                )}
+            </FetchComponent>
+        </>
+    )
+};
 
 
 
@@ -49,7 +51,7 @@ const ProductItem = styled.section`
         }
         p {
             font-size: var(--font-size-xxs);
-            color: var(--color-main-dark);
+            color: var(--color-dark);
         }
         h3 {
             font-size: var(--font-size-xs);
@@ -67,13 +69,13 @@ const ProductItem = styled.section`
             display: block;
             padding: 0;
             margin: 0;
-            aspect-ratio: 1/1;
-            object-fit: cover;
             overflow: hidden;
             border:1px solid #C4C4C4;
             border-radius: 10px;
             img {
                 width: 100%;
+                aspect-ratio: 1/1;
+                object-fit: cover;
                 display: block;
                 margin: 0;
             }
