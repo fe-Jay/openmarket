a66ad17cbb1fb8216217034e2f439d96cf2f1f59
jay <sinaelj@gmail.com>
2023-08-15 11:43:50 +0900

    ✨ feat: api 핸들러 함수 출력 파일 추가

--- src/ApiHandler.jsx
+++ src/ApiHandler.jsx
@@ -1,16 +1,31 @@
 /* eslint-disable camelcase */
+// eslint-disable-next-line import/namespace
 import useFetch from "./Hook/useFetch";
 
-export default function PostSignIn() {
-  const { response, error, isLoading, executeFetch } = useFetch();
+export function PostSignIn() {
+  const { response, error, executeFetch } = useFetch();
 
   const signIn = (username, password, login_type) => {
-    executeFetch("accounts/login", "POST", {
+    executeFetch("/accounts/login/", "POST", {
       username,
       password,
       login_type,
     });
   };
 
-  return { response, error, isLoading, signIn };
+  return { response, error, signIn };
 }
+
+export function GetProductList() {
+  const { response, error, executeFetch } = useFetch();
+
+  const getProducts = () => {
+    executeFetch("/products/");
+  };
+
+  return { response, error, getProducts };
+}
+
+// ✅ usage
+// import { function } from "../../ApiHandler";
+// const { response, error, function } = function();
