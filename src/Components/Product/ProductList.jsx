dc0e5eb65d31eb2512d8e8eea904b7f2282aa1b9
jay <sinaelj@gmail.com>
2023-08-15 11:44:24 +0900

    ✨ feat: 상품리스트 보기 api 추가

--- src/Components/Product/ProductList.jsx
+++ src/Components/Product/ProductList.jsx
@@ -1,40 +1,52 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useEffect } from "react";
 import { useLocation, Link } from "react-router-dom";
 import styled from "styled-components";
 
+import { GetProductList } from "../../ApiHandler";
 import { A11yHidden } from "../common/A11yHidden";
-import FetchComponent from "../common/FetchComponent";
 
 export default function ProductList() {
+  const { response, error, getProducts } = GetProductList();
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  useEffect(() => {
+    if (response) {
+      console.table(response);
+    }
+    if (error) {
+      console.error(error);
+    }
+  }, [response, error]);
+
+  // console.log(response);
   return (
-    <FetchComponent url="products/">
-      {data => (
-        <ProductItem>
-          <h2>
-            <A11yHidden>상품리스트</A11yHidden>
-          </h2>
-          {data && data.results && Array.isArray(data.results) ? (
-            data.results.map(prod => (
-              <article key={prod.product_id}>
-                <Link to={`/${prod.product_id}`}>
-                  <span>
-                    <img src={prod.image} alt={prod.product_name} />
-                  </span>
-                  <p>{prod.store_name}</p>
-                  <h3>{prod.product_name}</h3>
-                  <h4>
-                    <strong>{prod.price}</strong>원
-                  </h4>
-                </Link>
-              </article>
-            ))
-          ) : (
-            <div>상품이 없습니다.</div>
-          )}
-        </ProductItem>
+    <ProductItem>
+      <h2>
+        <A11yHidden>상품리스트</A11yHidden>
+      </h2>
+      {response && response.results && Array.isArray(response.results) ? (
+        response.results.map(prod => (
+          <article key={prod.product_id}>
+            <Link to={`/${prod.product_id}`}>
+              <span>
+                <img src={prod.image} alt={prod.product_name} />
+              </span>
+              <p>{prod.store_name}</p>
+              <h3>{prod.product_name}</h3>
+              <h4>
+                <strong>{prod.price}</strong>원
+              </h4>
+            </Link>
+          </article>
+        ))
+      ) : (
+        <div>상품이 없습니다.</div>
       )}
-    </FetchComponent>
+    </ProductItem>
   );
 }
 
