61a1d065b12759589e099b16b6f5750eb9d210ee
jay <sinaelj@gmail.com>
2023-08-23 17:34:21 +0900

    ✨ feat: 장바구니 수량 변경 리코일 구독형식으로 수정

--- src/Components/Product/ProductDetails.jsx
+++ src/Components/Product/ProductDetails.jsx
@@ -2,12 +2,20 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { useRecoilState, useRecoilValue } from "recoil";
 
 import { GetProductDetail, PostCartQuantity } from "../../ApiHandler";
-import { A11yHidden } from "../common/A11yHidden";
+import { countAtom, priceAtom, stockAtom } from "../../recoil/cartCountAtom";
+import { totalPrice } from "../../recoil/cartCountSelector";
+import A11yHidden from "../common/A11yHidden";
+import Button from "../common/Button";
+import Counter from "../common/Counter";
 
 export default function ProductDetails() {
   const { productId } = useParams();
+  // const count = useRecoilValue(countAtom);
+  const [quantity, setQuantity] = useRecoilState(countAtom);
+  const total = useRecoilValue(totalPrice);
 
   // * 상품 상세 페이지 데이터 호출
   const { response: prod, callApi: getProdId } = GetProductDetail(productId);
@@ -26,41 +34,37 @@ export default function ProductDetails() {
     image,
   } = prod || {};
 
-  // * 장바구니 수량 변경
-  const [unitPrice, setUnitPrice] = useState(price);
-  const [count, setCount] = useState(1);
-  const [stock, setStock] = useState(true);
-
-  const handleCartCount = e => {
-    const buttonValue = e.target.value;
-    let cartQuantity = count;
-
-    if (buttonValue === "minus" && count > 1) {
-      cartQuantity = count - 1;
-    } else if (buttonValue === "plus") {
-      cartQuantity = count + 1;
-    }
-    setCount(cartQuantity);
-    setUnitPrice(cartQuantity * price);
-  };
-
   // * 장바구니 담기
+  // 장바구니 담을 수량
+  const [unitPrice, setUnitPrice] = useRecoilState(priceAtom);
+  const [stock, setStock] = useRecoilState(stockAtom);
   const [data, setData] = useState({
     product_id: Number(productId),
-    quantity: count,
-    check: stock,
+    quantity,
+    check: 1,
   });
 
+  useEffect(() => {
+    if (prod && prod.price) {
+      setUnitPrice(prod.price);
+    }
+  }, [prod, setUnitPrice]);
+
+  useEffect(() => {
+    setData(prevData => ({ ...prevData, quantity }));
+  }, [quantity, total]);
+
+  // 장바구니 담기 API 호출
   const { response: cartInt, callApi: postCartInt } = PostCartQuantity();
   const handleCartProdAdd = () => {
     postCartInt(data);
+    console.warn(data.product_id, data.quantity, data.check);
   };
 
   return (
     <section>
       <h2>
         <A11yHidden>상품 상세</A11yHidden>
-        <p>test</p>
       </h2>
       {prod && (
         <article key={product_id}>
@@ -78,31 +82,18 @@ export default function ProductDetails() {
         </article>
       )}
 
-      <article>
-        <h2>
-          <A11yHidden>상품 수량 선택</A11yHidden>
-        </h2>
-        <button type="button" value="minus" onClick={e => handleCartCount(e)}>
-          -
-        </button>
-        <span>{count}</span>
-        <button type="button" value="plus" onClick={e => handleCartCount(e)}>
-          +
-        </button>
-      </article>
+      <Counter />
 
       <article>
         <h2>총 상품금액</h2>
-        <p>
-          총 수량
-          <strong>{count}</strong>개
-        </p>
-        <p>{unitPrice}</p>
+        <p>{total}</p>
       </article>
-      <button type="button">바로 구매</button>
-      <button type="button" onClick={handleCartProdAdd}>
+      <Button size="md" variant="primary">
+        바로 구매
+      </Button>
+      <Button size="md" variant="primary" onClick={handleCartProdAdd}>
         장바구니
-      </button>
+      </Button>
     </section>
   );
 }
